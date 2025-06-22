a49c8c2280a79db458bacf00305bbcd0c2666a0d
Ritesh-o4coder <fnaticritesh2004@gmail.com>
2025-06-22 20:07:38 +0530

    changes in hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,13 +6,16 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
-      url: process.env.SEPOLIA_URL,
-      accounts: [process.env.PRIVATE_KEY],
+      url: process.env.SEPOLIA_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545", // for local Hardhat node
     },
   },
   etherscan: {
     apiKey: {
-      sepolia: process.env.ETHERSCAN_API_KEY,
+      sepolia: process.env.ETHERSCAN_API_KEY || "",
     },
   },
 };
